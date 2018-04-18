546295a24853b321df22f0eb04ddc01cd812cd79
Eric Prescott <edp109@cse.psu.edu>
2018-04-18 14:23:29 -0400

    added comments, and else test

--- motion_sensor.py
+++ motion_sensor.py
@@ -1,30 +1,48 @@
 #!/usr/bin/env python
 
+# get GPIO library
 import RPi.GPIO as GPIO
+# get time library
 import time
+
+# set gpio pins for various components
+
+# pasive IR sensor
 PIR = 17
+# Red LED
 R_LED = 21
+# Green LED
 G_LED = 18
+
+#setup GPIO modes and initalize GPIO Pins for use.
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(R_LED,GPIO.OUT)
 GPIO.setup(G_LED,GPIO.OUT)
 GPIO.setup(PIR, GPIO.IN)
 
+# Set the current state for PIR to 0 not detecting
 current_state = 0
 try:
+
+    # while True loop.
     while True:
+
+	# pause for a 
         time.sleep(0.1)
+	# get state from PIR
         current_state = GPIO.input(PIR)
-        if current_state == 1:
+        # If motion detected
+	if current_state == 1:
             print("GPIO pin %s is %s" % (PIR, current_state))
             print("Motion Detected")
             GPIO.output(R_LED,GPIO.HIGH)
             GPIO.output(G_LED,GPIO.LOW)
             time.sleep(1)
+	else:
             print("Motion clear")
             GPIO.output(R_LED,GPIO.LOW)
             GPIO.output(G_LED,GPIO.HIGH)
-            time.sleep(5)
+            time.sleep(1)
 except KeyboardInterrupt:
     pass
 finally:
